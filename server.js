23bfaec840f4742bdd60783c80577586caa3a8a3
RaffaBassman <raffabassman@users.noreply.github.com>
2020-10-26 09:34:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-round-48907',
-		user: 'postgres',
-		password: 'Slon2012',
-		database: 'smart-brain'
+		host: process.env.DATABASE_URL,
+		ssl: true;
 	}
 });
 
